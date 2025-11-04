5640691d5f423a93f05b470b5f469bf6a4f4593d
Varun Kumar Maheshwari <varunmaheswari18052019@gmail.com>
2025-11-04 20:44:59 +0530

    did some things

--- dark-mode-toggle/script/script.js
+++ dark-mode-toggle/script/script.js
@@ -1,5 +1,23 @@
 const theme_btn = document.querySelector(".theme-btn");
+const bodyComputedStyle = window.getComputedStyle(document.body);
+const color = 'rgb(189, 167, 167)'; 
+const backGroundColor = bodyComputedStyle.backgroundColor;
+checkDarkMode(backGroundColor, color)
+console.log(document.body.className);
+
+
+
+
 
 theme_btn.addEventListener('click' , () => {
+    const backGroundColor = bodyComputedStyle.backgroundColor;
     document.body.classList.toggle('dark-mode');
+    checkDarkMode(backGroundColor, color)
 })
+
+function checkDarkMode(backGroundColor,color) {
+    if(document.body.className === "dark-mode"){
+        theme_btn.innerText = 'Light Mode';
+    }
+    else theme_btn.innerText = 'Dark Mode';
+}
